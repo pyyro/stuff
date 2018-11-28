167d774bdf7257a02a896fda596c5343ba9eb66e
pyyro <30564925+pyyro@users.noreply.github.com>
2018-11-28 14:14:09 +0545

    Update newegg-scraper.py

--- newegg-scraper.py
+++ newegg-scraper.py
@@ -1,3 +1,11 @@
+"""
+this used to work when i wrote this
+now it doesnt, atleast on my machine
+maybe newegg changed their code of the product containers
+or maybe my laptop is retarded now
+or maybe im retarded
+"""
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -31,4 +39,6 @@ def listItems(keyword):
 print('code author: yahikodark')
 listItems(input('Search for: '))
 
-k = input('Press Enter to quit')
+k = input('Press Enter to quit') #forgot why i added this
+                                 #probably to have the terminal ask for a input instead of immediately quiting after listing
+                                 #the results... idk i forgot 
