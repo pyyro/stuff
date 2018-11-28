ecf8e6ef05b139b0e1da81ad37223ed98c8507b1
pyyro <30564925+pyyro@users.noreply.github.com>
2018-11-28 14:07:08 +0545

    Add files via upload

---
+++ newegg-scraper.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def listItems(keyword):
+    url = 'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=' + str(keyword) + '&N=-1&isNodeId=1'
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'html.parser')
+    containers = soup.findAll('div', {'class': 'item-container'})
+    try:
+
+        for container in containers:
+            brand = container.div.div.a.img['title']
+
+            name_container = container.find('a', {'class': 'item-title'})
+            name = name_container.text
+
+            price_cont_finder = container.find('div', {'class': 'item-action'})
+            price_container = price_cont_finder.find('strong')
+            price = price_container.text
+
+            print('Brand: ' + brand)
+            print('Name ' + name)
+            print('Price: ' + '$' + price)
+            print('================================================')
+    except:
+        print('some retarded error, code still works.')
+
+
+print('code author: yahikodark')
+listItems(input('Search for: '))
+
+k = input('Press Enter to quit')
