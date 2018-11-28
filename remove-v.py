62497cacbd69664850f73a200d6370cf10119df1
pyyro <30564925+pyyro@users.noreply.github.com>
2018-11-28 14:18:20 +0545

    Add files via upload

---
+++ remove-v.py
@@ -0,0 +1,19 @@
+VOWELS = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+text = input('Enter your text here: ')
+
+
+final_text = 'Final Text: ' + ''
+removed_vowels = 'Removed Vowels are: ' + ''
+
+for letter in text:
+    if letter not in VOWELS:
+        final_text += letter
+
+    if letter in VOWELS:
+        removed_vowels += letter
+
+print(final_text)
+print(removed_vowels)
+
+
+
